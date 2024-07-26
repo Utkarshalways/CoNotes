c42db568cd9502769c7cd515627ff06f3b4f583d
Utkarshalways <utkarshshrma08@gmail.com>
2024-07-26 21:46:01 +0530

    header removed

--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div className='border-b border-b-black w-full p-2  flex  items-center text-xl'>Header</div>
-  )
-}
-
-export default Header
